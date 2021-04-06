329130947552270cd000feedb823b5f26f13579e
Tirhas-source <56926891+Tirhas-source@users.noreply.github.com>
2021-04-05 20:48:49 -0400

    commit T

--- Covid-19/app.py
+++ Covid-19/app.py
@@ -1,4 +1,36 @@
-
-import functions 
+from flask import Flask 
+from functions import *
 import pandas as pd 
 df = open_as_df("select * from state_history;", connect(param_dic))
+
+
+# Create an instance of Flask
+app = Flask(__name__)
+
+# Route to render index.html template using data from Mongo
+@app.route("/")
+def home():
+
+    # Find one record of data from the database
+    destination_data = mongo.db.collection.find_one()
+
+    # Return template and data
+    return render_template("index.html", vacation=destination_data)
+
+
+# Route that will trigger the scrape function
+@app.route("/scrape")
+def scrape():
+
+    # Run the scrape function
+    costa_data = scrape_costa.scrape_info()
+
+    # Update the Mongo database using update and upsert=True
+    mongo.db.collection.update({}, costa_data, upsert=True)
+
+    # Redirect back to home page
+    return redirect("/")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
